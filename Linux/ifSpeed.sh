db875e49d61e2fa59c26b66de0b3ecdb32405dfe
jmoreis <39521465+jmoreis@users.noreply.github.com>
2023-05-23 12:07:22 -0300

    Update ifSpeed.sh

--- Linux/ifSpeed.sh
+++ Linux/ifSpeed.sh
@@ -3,7 +3,6 @@
 echo "Velocidade das interfaces:"
 for Int in `ls -1 /sys/class/net`
 do
-    #if [ $Int != "lo" ]
     if echo $Int|egrep '^e' 1>/dev/null
     then
         echo -e "\t$Int = `cat /sys/class/net/$Int/speed` Mb/s"
